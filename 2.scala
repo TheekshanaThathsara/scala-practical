8eba3fc4e5ceeba416ffd3f61080ba297a8fcce5
Theekshana Thathsara <140165416+TheekshanaThathsara@users.noreply.github.com>
2024-07-23 01:28:59 +0530

    2.scala

--- 2.scala
+++ 2.scala
@@ -1,25 +1,47 @@
-object Main extends App {
-  var a, b, c, d: Int = 0
-  var k: Float = 0.0f
+object VariableEvaluation {
+  def main(args: Array[String]): Unit = {
+    var a = 2
+    var b = 3
+    var c = 4
+    var d = 5
+    val k = 4.3f
+    var g = 0.0f // Assuming g needs to be declared
 
-  // Assign initial values
-  a = 2
-  b = 3
-  c = 4
-  d = 5
-  k = 4.3f
+    // Evaluate the expressions
 
-  // Evaluate expressions
-  val expression_a = -(-b) * a + c * d - (-1)
-  val expression_b = { val temp = a; a += 1; temp }
-  val expression_c = -(2 * (g - k)) + c  // Assuming 'g' is another variable
-  val expression_d = { c = c + 1; c }    // Post-increment assignment to 'c'
-  val expression_e = { c = c + 1; c * a }
+    // a) println( - -b * a + c * d - -);
+    // This expression appears to have an error with the final - -. I'll assume it should be -(-b * a) + c * d
+    println(-(-b * a) + c * d)  // Corrected expression
 
-  // Print results
-  println(s"a) Result of - -b * a + c * d - -: $expression_a")
-  println(s"b) Result of a++: $expression_b")
-  println(s"c) Result of –2 * (g – k) + c: $expression_c")
-  println(s"d) Result of c=c++: $expression_d")
-  println(s"e) Result of c=++c*a++: $expression_e")
+    // b) println(a++);
+    println({
+      val temp = a
+      a += 1
+      temp
+    }) // Scala does not support post-increment (a++), so we use a temporary variable
+
+    // c) println(–2 * ( g – k ) +c);
+    // The expression uses en-dash instead of a hyphen for negative sign. I'll correct it.
+    println(-2 * (g - k) + c)
+
+    // d) println(c=c++);
+    // Scala does not support post-increment (c++), so we use a temporary variable
+    println({
+      val temp = c
+      c += 1
+      temp
+    })
+
+    // e) println (c=++c*a++);
+    // Scala does not support pre-increment (++c) or post-increment (a++), so we use a block
+    println({
+      c += 1
+      val result = c * {
+        val temp = a
+        a += 1
+        temp
+      }
+      result
+    })
+  }
 }
