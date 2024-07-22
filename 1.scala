d9f323ae2fb709e4728df83a8335ea8134f7e800
Theekshana Thathsara <140165416+TheekshanaThathsara@users.noreply.github.com>
2024-07-23 01:28:17 +0530

    1.scala

--- 1.scala
+++ 1.scala
@@ -1,28 +1,24 @@
-object Main extends App {
-  var i, j, m, n, k: Int = 0
-  var f, g: Float = 0.0f
-  var c: Char = ' '
+object VariableEvaluation {
+  def main(args: Array[String]): Unit = {
+    var i, j, k = 2
+    val m, n = 5
+    val f = 12.0f
+    val g = 4.0f
+    val c = 'X'
 
-  // Assign initial values
-  k = i = j = 2
-  m = n = 5
-  f = 12.0f
-  g = 4.0f
-  c = 'X'
+    // Evaluate the expressions
+    val a = k + 12 * m
+    val b = m / j
+    val d = m / j * j
+    val e = f + 10 * 5 + g
+    val fNew = { i += 1; i } * n
 
-  // Evaluate expressions
-  val a = k + 12 * m
-  val b = m / j
-  val c1 = n % j
-  val d = m / j * j
-  val e = f + 10 * 5 + g
-  val f1 = (++i) * n
-
-  // Print results
-  println(s"a) k + 12 * m = $a")
-  println(s"b) m / j = $b")
-  println(s"c) n % j = $c1")
-  println(s"d) m / j * j = $d")
-  println(s"e) f + 10*5 + g = $e")
-  println(s"f) ++i * n = $f1")
+    // Print the results
+    println(s"a) k + 12 * m = $a")
+    println(s"b) m / j = $b")
+    println(s"c) n % j = ${n % j}")
+    println(s"d) m / j * j = $d")
+    println(s"e) f + 10 * 5 + g = $e")
+    println(s"f) ++i * n = $fNew")
+  }
 }
